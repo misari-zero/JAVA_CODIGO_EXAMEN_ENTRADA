0c4043a9019c07c17fceba0c9418eb8ba0361f34
Gianfranco Misari <gianfranco.misari.contractor@bbva.com>
2024-04-03 16:38:27 -0500

    Primer avance - Examen Entrada

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         List<Empleado> empleados = new ArrayList<>();
         // Desarrollador
         empleados.add(new Desarrollador(1, "Juan", "Tecnologia", 3000, List.of("Java", "Python")));
